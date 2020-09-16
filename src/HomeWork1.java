0f656208111eaabcb89cfe91bdf4a5a9f0bc4a99
Anna Shershneva <annashershneva@Annas-Air.fios-router.home>
2020-09-16 16:06:57 -0400

    HomeWork1

---
+++ src/HomeWork1.java
@@ -0,0 +1,42 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        byte numberOfHours = 24;
+        System.out.println("There are " + numberOfHours + " hours in a day.");
+
+        short maxNoOfDays = 366;
+        System.out.print("Maximum number of days in a year is " + maxNoOfDays + ".");
+
+        int totalNoOfEmployees = 33458;
+        System.out.println("There are a total of " + totalNoOfEmployees + " employees in this organization.");
+
+        long populationOfTheCountry = 19407468365243L;
+        System.out.println("Population of the country is " + populationOfTheCountry + ".");
+
+        float interestRate = 3.04f;
+        System.out.println("The average rate for a 30-year fixed mortgage is " + interestRate + " percent.");
+
+        double availableBalance = 56483.89;
+        System.out.println("Your current account balance is " + availableBalance + "$.");
+
+        boolean sunRise = false;
+        System.out.println("Does the sun rise from the west?" + ' ' + sunRise + ".");
+
+        char firstInitial = 'A';
+        char secondInitial = 'S';
+        System.out.println("My initials is " + firstInitial + secondInitial + ".");
+
+        String myName = "Anna Shershneva";
+        System.out.print("My name is " + myName + ".");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/Test/TestClass.java
+++ src/Test/TestClass.java
@@ -1,4 +1,12 @@
 package Test;
 
 public class TestClass {
+    public static void main(String[] args) {
+        byte myCatAge = 7;
+        System.out.println(myCatAge);
+
+
+    }
+
+
 }
