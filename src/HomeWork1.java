097663527fa8a36430527f002e5929fea97ca1c1
Anna Shershneva <annashershneva@Annas-Air.fios-router.home>
2020-09-20 20:07:17 -0400

    HomeWork2

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -4,7 +4,7 @@ public class HomeWork1 {
         System.out.println("There are " + numberOfHours + " hours in a day.");
 
         short maxNoOfDays = 366;
-        System.out.print("Maximum number of days in a year is " + maxNoOfDays + ".");
+        System.out.println("Maximum number of days in a year is " + maxNoOfDays + ".");
 
         int totalNoOfEmployees = 33458;
         System.out.println("There are a total of " + totalNoOfEmployees + " employees in this organization.");
@@ -26,7 +26,41 @@ public class HomeWork1 {
         System.out.println("My initials is " + firstInitial + secondInitial + ".");
 
         String myName = "Anna Shershneva";
-        System.out.print("My name is " + myName + ".");
+        System.out.println("My name is " + myName + ".");
+
+
+
+        byte km = 1; // 1 miles to km = 1.60934 km
+        double mile = 1.609 * km;
+        boolean is1MileEqTo1Km = km == mile;
+        System.out.println("Is 1 km equal to 1 mile? "+ is1MileEqTo1Km);
+
+
+        // 4 hours contain 14400 seconds ?
+
+
+        int noOfHours = 4;
+        int noOfSec = 14400;
+        int noOfHoursToSecConv = noOfHours * 3600;
+        boolean isNoOfHoursEqToNoOfSec = noOfHoursToSecConv == noOfSec;
+        System.out.println("Do " + noOfHours + " hours contain " + noOfSec + " seconds? " + isNoOfHoursEqToNoOfSec);
+
+
+        // find if given number is even
+        int num = 10;
+        int result = num % 2;
+        int shouldBe = 0;
+
+        boolean isEven = result == shouldBe;
+        System.out.println("Is " + num + " even? " + isEven);
+
+        /*
+            code
+            System.out.println(num + " is even number: " + isEven)
+         */
+
+
+
 
 
 
---
+++ src/HomeWork2.java
@@ -0,0 +1,38 @@
+public class HomeWork2 {
+    public static void main(String[] args) {
+
+        // F -> C; T(°C) = (T(°F) - 32) × 5/9;
+        double tempFahrenheitForCelsius = 100;
+        double tempCelsiusFromF = (tempFahrenheitForCelsius -32) * (5.0/9.0);
+        System.out.println(tempFahrenheitForCelsius + " F is equal to " + tempCelsiusFromF + " degrees Celsius.");
+
+        //F -> K; T(K) = (T(°F) + 459.67)× 5/9;
+        double tempFahrenheitForKelvin = 100;
+        double tempKelvinFromFahrenheit = (tempFahrenheitForKelvin + 459.67) * (5.0/9.0);
+        System.out.println(tempFahrenheitForKelvin + " F is equal to " + tempKelvinFromFahrenheit + " degrees Kelvin.");
+
+        //K -> C; T(°C) = T(K) - 273.15;
+        double tempKelvinForCelsius = 100;
+        double tempCelsiusFromKelvin = tempKelvinForCelsius- 273.15;
+        System.out.println(tempKelvinForCelsius + " K is equal to " + tempCelsiusFromKelvin + " degrees Celsius.");
+
+        //K -> F; T(°F) = T(K) × 9/5 - 459.67
+        double tempKelvinForFahrenheit = 100;
+        double tempFahrenheitFromKelvin = (tempKelvinForFahrenheit * (9.0/5.0)) - 459.67;
+        System.out.println(tempKelvinForFahrenheit + " K is equal to " + tempFahrenheitFromKelvin + " degrees Fahrenheit.");
+
+        //C -> F; T(°F) = T(°C) × 1.8 + 32;
+        double tempCelsiusForFahrenheit = 100;
+        double tempFahrenheitFromCelsius = (tempCelsiusForFahrenheit * 1.8) + 32;
+        System.out.println(tempCelsiusForFahrenheit + " C is equal to " + tempFahrenheitFromCelsius + " degrees Fahrenheit.");
+
+        //C -> K; T(K) = T(°C) + 273.15;
+        double tempCelsiusForKelvin = 100;
+        double tempKelvinFromCelsius = tempCelsiusForKelvin + 273.15;
+        System.out.println(tempCelsiusForKelvin + " C is equal to " + tempKelvinFromCelsius + " degrees Kelvin.");
+
+
+
+
+    }
+}
